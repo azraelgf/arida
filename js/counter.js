ee87f7d9d3a492ce5bad93549174e675b49ddbca
Oleg <aleh.antukh@gmail.com>
2025-10-29 15:17:52 +0300

    v2.0

--- js/counter.js
+++ js/counter.js
@@ -1,26 +1,52 @@
 (() => {
     "use strict";
     function formQuantity() {
-        document.addEventListener("click", function(e) {
+        document.addEventListener("click", function (e) {
             const targetElement = e.target;
-            if (targetElement.closest("[data-quantity-plus]") || targetElement.closest("[data-quantity-minus]")) {
-                const quantityBlock = targetElement.closest("[data-quantity]");
-                const valueElement = quantityBlock.querySelector("[data-quantity-value]");
+            if (targetElement.closest('[data-quantity-plus]') || targetElement.closest('[data-quantity-minus]')) {
+                const quantityBlock = targetElement.closest('[data-quantity]');
+                const valueElement = quantityBlock.querySelector('[data-quantity-value]');
                 let value = parseInt(valueElement.value);
-                if (targetElement.hasAttribute("data-quantity-plus")) {
+
+                if (targetElement.hasAttribute('data-quantity-plus')) {
                     value++;
-                    if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) value = +valueElement.dataset.quantityMax;
+                    if (+valueElement.dataset.quantityMax && +valueElement.dataset.quantityMax < value) {
+                        value = +valueElement.dataset.quantityMax;
+                    }
                 } else {
                     value--;
                     if (+valueElement.dataset.quantityMin) {
-                        if (+valueElement.dataset.quantityMin > value) value = +valueElement.dataset.quantityMin;
-                    } else if (value < 1) value = 1;
+                        if (+valueElement.dataset.quantityMin > value) {
+                            value = +valueElement.dataset.quantityMin;
+                        }
+                    } else if (value < 1) {
+                        value = 1;
+                    }
                 }
+
                 valueElement.value = value;
-                const productActions = quantityBlock.closest(".product")?.querySelector(".product__actions");
-                if (productActions) if (value === 1) productActions.classList.add("_qty-noactive"); else productActions.classList.remove("_qty-noactive");
+
+                // Находим actions-блок
+                const productActions = quantityBlock.closest('.product')?.querySelector('.product__actions');
+                if (productActions) {
+                    if (value === 1) {
+                        productActions.classList.add('_qty-noactive');
+                    } else {
+                        productActions.classList.remove('_qty-noactive');
+                    }
+                }
+                // Находим actions-блок
+                const productActions2 = quantityBlock.closest('.product-card__info')?.querySelector('.product-info__actions');
+                if (productActions2) {
+                    if (value === 1) {
+                        productActions2.classList.add('_qty-noactive');
+                    } else {
+                        productActions2.classList.remove('_qty-noactive');
+                    }
+                }
             }
         });
+
     }
     formQuantity();
 })();
